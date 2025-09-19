f08d5a36098254377a41725199a2f60b6c21e8a4
adventurine-w <flexingcomp@outlook.com>
2025-09-19 18:29:34 +0800

    added isInteger

--- QuestType.java
+++ QuestType.java
@@ -16,19 +16,19 @@ import static org.adv.clickerflex.utils.generic.SF.sf;
 public enum QuestType {
     KILL_MOBS(goal->{
         return "<7>Kill <c>"+sf(goal)+" <7>mobs";
-    }, ItemCreator.of(Material.IRON_SWORD), new QuestKillMobs()),
+    }, ItemCreator.of(Material.IRON_SWORD), new QuestKillMobs(), true),
     DEAL_DAMAGE(goal->{
         return "<7>Deal <c>"+sf(goal)+" <7>damage";
-    }, ItemCreator.of(Material.STICK), new QuestDealDamage()),
+    }, ItemCreator.of(Material.STICK), new QuestDealDamage(), true),
     DEAL_CRIT_DAMAGE(goal->{
         return "<7>Deal <c_red>"+sf(goal)+" <7>critical damage";
-    }, ItemCreator.of(Material.PARROT_SPAWN_EGG), new QuestDealCritDamage()),
+    }, ItemCreator.of(Material.PARROT_SPAWN_EGG), new QuestDealCritDamage(), true),
     GAIN_EXP(goal->{
         return "<7>Gain <3>"+sf(goal)+" <7>EXP";
-    }, ItemCreator.of(Material.PARROT_SPAWN_EGG), new QuestDealCritDamage()),
+    }, ItemCreator.of(Material.PARROT_SPAWN_EGG), new QuestGainEXP(), true),
     MOVE_BLOCKS(goal->{
         return "<7>Move <a>"+sf(goal)+" <7>blocks";
-    }, ItemCreator.of(Material.DIAMOND_BOOTS), new QuestMoveBlocks()),
+    }, ItemCreator.of(Material.DIAMOND_BOOTS), new QuestMoveBlocks(), true),
     KILL_BOSS(goal->{
         return "<c>Kill The Boss!";
     }, type->ItemCreator.of(type.getSprite()), new QuestKillBoss(), true),
@@ -40,21 +40,24 @@ public enum QuestType {
     @Getter
     private final QuestTypeAbstr instance;
     private final boolean disallowAllPredicate;
+    @Getter
+    private final boolean integer;
 
-    QuestType(Function<Double, String> descF, ItemCreator sprite, QuestTypeAbstr instance) {
-        this(descF, x->sprite, instance, false);
+    QuestType(Function<Double, String> descF, ItemCreator sprite, QuestTypeAbstr instance, boolean integer) {
+        this(descF, x->sprite, instance, false, integer);
     }
-    QuestType(Function<Double, String> descF, Function<ChallengerType, ItemCreator> spriteF, QuestTypeAbstr instance) {
-        this(descF, spriteF, instance, false);
+    QuestType(Function<Double, String> descF, Function<ChallengerType, ItemCreator> spriteF, QuestTypeAbstr instance, boolean integer) {
+        this(descF, spriteF, instance, false, integer);
     }
-    QuestType(Function<Double, String> descF, ItemCreator sprite, QuestTypeAbstr instance, boolean disallowAllPredicate) {
-        this(descF, x->sprite, instance, disallowAllPredicate);
+    QuestType(Function<Double, String> descF, ItemCreator sprite, QuestTypeAbstr instance, boolean disallowAllPredicate, boolean integer) {
+        this(descF, x->sprite, instance, disallowAllPredicate, integer);
     }
-    QuestType(Function<Double, String> descF, Function<ChallengerType, ItemCreator> spriteF, QuestTypeAbstr instance, boolean disallowAllPredicate) {
+    QuestType(Function<Double, String> descF, Function<ChallengerType, ItemCreator> spriteF, QuestTypeAbstr instance, boolean disallowAllPredicate, boolean integer) {
         this.descF = descF;
         this.spriteF = spriteF;
         this.instance = instance;
         this.disallowAllPredicate = disallowAllPredicate;
+        this.integer = integer;
     }
     public String getDescription(double goal){
         return descF.apply(goal);
@@ -66,3 +69,4 @@ public enum QuestType {
         return !disallowAllPredicate || instance instanceof QuestPredicateExtractor<?>;
     }
 }
+
